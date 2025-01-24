a5b2a1029b5cf6cdf34cb1cb7ba7112a8262fdca
sahil <sahilpvns@gmail.com>
2025-01-24 20:47:56 +0530

    UserID fix

--- app/src/main/java/com/zerobrokage/allservices/homeservices/vehiclesservices/electricians/allhomeservices/zerobrokage/ui/activity/SubMenuActivity.kt
+++ app/src/main/java/com/zerobrokage/allservices/homeservices/vehiclesservices/electricians/allhomeservices/zerobrokage/ui/activity/SubMenuActivity.kt
@@ -27,7 +27,7 @@ class SubMenuActivity : AppCompatActivity(), ItemClickListener {
         setContentView(binding.root)
 
         sharedPref = getSharedPreferences("MyPrefs", MODE_PRIVATE)
-        val userId = getSharedPreferences("MyPrefs", MODE_PRIVATE).getInt("userId", 0)
+        val userId = sharedPref.getInt("userId",0)
 
         val menuID = intent.getIntExtra("id", 0)
         val name = intent.getStringExtra("name") ?: "Unknown Service"
